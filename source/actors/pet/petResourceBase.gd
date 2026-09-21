040cfd2b47efa90fc1b8c03ec6b03f22c60c8c38
Kingcrimson <rylanmease27@gmail.com>
2026-09-20 22:00:34 -0400

    Branch edit
    I added my branch edit

--- source/actors/pet/petResourceBase.gd
+++ source/actors/pet/petResourceBase.gd
@@ -8,5 +8,7 @@ enum AnimalType { Monkey, Capybara }
 @export var texture: Texture2D
 @export var name: String = 'Grumpel'
 
+# Feature Branch Edit: Added by [Rylan Mease] for Assignment 2
+
 func get_animal_type_name():
 	return AnimalType.keys()[animal]
