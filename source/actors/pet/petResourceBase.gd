62fb11b10d4ccb698c0397338444bdfd6f1fde87
Kingcrimson <rylanmease27@gmail.com>
2026-09-24 01:26:50 -0400

    Added import and png
    I added the rat png and added the import code.

--- source/actors/pet/petResourceBase.gd
+++ source/actors/pet/petResourceBase.gd
@@ -2,13 +2,12 @@ extends Resource
 
 class_name petResource
 
-enum AnimalType { Monkey, Capybara }
+enum AnimalType { Rat, Racoon }
+# Feature Branch Edit: Added by [Rylan Mease] for Assignment 2
 
 @export var animal: AnimalType
 @export var texture: Texture2D
 @export var name: String = 'Grumpel'
-
-# Feature Branch Edit: Added by [Rylan Mease] for Assignment 2
 
 func get_animal_type_name():
 	return AnimalType.keys()[animal]
